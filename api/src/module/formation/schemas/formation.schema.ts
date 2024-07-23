dba0549b6d63a210bd792f2c1573252176d8cfa9
mahmoudbecheikh <becheikh.mahmoud.3@gmail.com>
2024-07-23 11:54:42 +0100

    fix all

--- api/src/module/formation/schemas/formation.schema.ts
+++ api/src/module/formation/schemas/formation.schema.ts
@@ -8,15 +8,9 @@ import { Niveau, NiveauSchema } from 'src/module/niveau/schemas/niveau.schema';
 export type FormationDocument = HydratedDocument<Formation>;
 
 @Schema()
-<<<<<<< HEAD
 export class Formation {
   @Prop({ required: true })
   formationId: string;
-=======
-export class Formation extends Document {
-  /*@Prop({ required: true })
-  formationId: string;*/
->>>>>>> 58531d352d973382d3660ac7002f9a0f19050a7c
 
   @Prop({ required: true })
   titre: string;
@@ -27,25 +21,22 @@ export class Formation extends Document {
   @Prop({ required: true })
   description: string;
 
-  @Prop({required:true})
-  competenceAquises:string;
+  @Prop({ required: true })
+  competenceAquises: string;
 
-  @Prop({required:true})
-  resultatSouhaites:string;
+  @Prop({ required: true })
+  resultatSouhaites: string;
 
-  @Prop({required:true})
-  nbrNiveau:number;
+  @Prop({ required: true })
+  nbrNiveau: number;
 
-<<<<<<< HEAD
   @Prop({ type: [NiveauSchema], default: [] })
   niveaux: Types.DocumentArray<Niveau>;
 
   @Prop({ type: [MessageSchema], default: [] })
   forums: Types.DocumentArray<Message>;
-=======
   @Prop({ type: [Types.ObjectId], ref: 'Niveau', required: true })
   niveau: Niveau[];
->>>>>>> 58531d352d973382d3660ac7002f9a0f19050a7c
 }
 
 export const FormationSchema = SchemaFactory.createForClass(Formation);
